5317a32be053fcd53c527eca4e466b9f0f5ae22f
brandiux <brandon.valenzuela247077@potros.itson.edu.mx>
2025-09-15 12:00:34 -0700

    Base del proyecto separada en 4 capas, cada capa en un proyecto por separado

---
+++ PersistenciaTutorias/src/main/java/com/mycompany/persistenciatutorias/PersistenciaTutorias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistenciatutorias;
+
+/**
+ *
+ * @author brand
+ */
+public class PersistenciaTutorias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
